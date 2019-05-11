af20d5eae88cd5b5e1c843ae365401d5092af1f5
vinod@711 <thupakulavinodkumar@711>
2019-05-11 22:57:19 +0530

    cric1

--- sports.py
+++ sports.py
@@ -2,5 +2,6 @@ print("I Like Playing cricket")
 sport=input("enter sport:")
 if sport=="cricket":
     print("goto the ground and play cricket")
+    print("u r  good player")
 else:
     print("do work in office")
