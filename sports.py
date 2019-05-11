1771bde1d5d1aa23ffebe0dc550b60b5a7ab42e7
vinod@711 <thupakulavinodkumar@711>
2019-05-11 22:51:02 +0530

    v1

--- sports.py
+++ sports.py
@@ -3,4 +3,7 @@ sport=input("enter sport:")
 if sport=="cricket":
     print("goto the ground and play cricket")
 else:
+    game=input("are u vallyball player")
+    if game=="yes":
+        print("play vallyball ")
     print("do work in office")
